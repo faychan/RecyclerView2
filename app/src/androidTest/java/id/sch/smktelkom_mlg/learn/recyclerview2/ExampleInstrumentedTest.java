8467306db51c09ec5ab0720e3457375aac9f83d0
Faychan <nor.iffat32@gmail.com>
2016-11-02 06:37:08 +0700

    -Initial Commit RecyclerView2

--- app/src/androidTest/java/id/sch/smktelkom_mlg/learn/recyclerview2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/id/sch/smktelkom_mlg/learn/recyclerview2/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package id.sch.smktelkom_mlg.learn.recyclerview1;
+package id.sch.smktelkom_mlg.learn.recyclerview2;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("id.sch.smktelkom_mlg.learn.recyclerview1", appContext.getPackageName());
+        assertEquals("id.sch.smktelkom_mlg.learn.recyclerview2", appContext.getPackageName());
     }
 }
